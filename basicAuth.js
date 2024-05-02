06599f5a57964dd567725e79195f84ff3e715c3f
Andrey <andrey6458@yandex.ru>
2024-05-02 20:27:13 +0500

    Сервер

---
+++ basicAuth.js
@@ -0,0 +1,21 @@
+const auth = require('basic-auth')
+
+const mwBasicAuth = async (req, res, next) => {
+    console.log('middleware: basic auth')
+
+    const user = await auth(req)
+
+    const username = 'test'
+    const password = '123456'
+
+    if (user && user.name.toLowerCase() === username.toLowerCase() && user.pass === password) {
+        console.log('Basic Auth: success')
+        next()
+    } else {
+        console.log('Basic Auth: failure')
+        res.statusCode = 401
+        res.end('Access denied')
+    }
+}
+
+export default mwBasicAuth
