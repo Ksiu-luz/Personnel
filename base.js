c7bd6f8ac50dcfb46f919db991b7ae1b17d4807e
SultanovSlava <sultanoffslav@gmail.com>
2024-05-07 22:27:58 +0500

    'norm_search_func_without_skill'

--- base.js
+++ base.js
@@ -21,10 +21,56 @@ conn.connect((err) => {
     if (err) throw err;
 });
 
-const findUserByTg = async (tg) => {
-    const result = await conn.query(`select student_name, password_hash from students where telegram_contact = '@${tg}' LIMIT 1`);
-    return result.rows[0];
+const findUsersByName = async (student_name) => {
+    const result = await conn.query(`SELECT * FROM students WHERE student_name = '${student_name}'`);
+    return result;
 }
 
-findUserByTg("Andrew").then((value) => {console.log(value.student_name)});
-exports.findUserByTg = findUserByTg;
+const findUsersByGroup = async (student_group) => {
+    const result = await conn.query(`SELECT * FROM students WHERE group_id = (SELECT group_id FROM groups WHERE group_num = '${student_group}')`);
+    return result;
+}
+
+const findUsersByStatus = async (student_status) => {
+    const result = await conn.query(`SELECT * FROM students WHERE status = '${student_status}'`);
+    return result;
+}
+
+const findUserById = async (student_id) => {
+    const result = await conn.query(`SELECT * FROM students WHERE student_id = '${student_id}'`);
+    return result;
+}
+
+const findUsersIdsBySkill = async (skill_name) => {
+    const result = await conn.query(`SELECT student_id FROM student_skills WHERE skill_id = (SELECT skill_id FROM skills WHERE skill_name = '${skill_name}')`);
+    return result;
+}
+
+const findUsersBySkills = async (skills) => {
+    let idsBySkills = [];
+    for (const skill_name of skills) {
+        findUsersIdsBySkill(skill_name).then((value) => {
+            let idsBySkill = [];
+            for (const row of value.rows) {
+                idsBySkill.push(+row.student_id);
+            }
+            idsBySkills.push(idsBySkill);
+        });
+    }
+    
+    const intersection = idsBySkills.reduce((accumulator, array) => {
+        return accumulator.length === 0 ? array : accumulator.filter(element => array.includes(element));
+    }, []);
+
+    console.log(intersection);
+
+    
+    return 0;
+}
+
+
+findUsersBySkills(['PSQL master', 'C# master']);
+//findUsersBySkills('PSQL master').then((value) => {console.log(value.rows)});
+exports.findUsersByName = findUsersByName;
+exports.findUsersByGroup = findUsersByGroup;
+exports.findUsersByStatus = findUsersByStatus;
