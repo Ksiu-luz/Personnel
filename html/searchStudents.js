f10d5178c10f224dbc81361076b41e9907baa963
fuwariko <cessedimeow@gmail.com>
2024-05-11 18:46:57 +0500

    улучшенный поиск

--- html/searchStudents.js
+++ html/searchStudents.js
@@ -83,7 +83,50 @@ function loadUsers(x) {
 
 };
 
+function loadUser(user) {
+  let e = document.querySelector('.results');
+
+  e.innerHTML = '';
+
+  
+      var userName = user.userName;
+      var group = user.group;
+      var skills = user.skills;
+      var personalInfo = user.personalInfo;
+      var statusImgSrc =user.statusImgSrc;
+      var statusAltText = user.statusAltText;
+  
+      var worksheet = document.createElement('section');
+      worksheet.className = 'worksheet';
+  
+      worksheet.innerHTML = `
+        <section onclick="window.open('login.html', '_blank')" class="profile-info">
+          <div class="block">
+            <img src="images/avatar.jpg" alt="Фото" class="profile-img">
+            <article class="user">
+              <div class="info">
+                <div class="user-info">
+                  <h1>${userName}</h1>
+                  <p class="group">${group}</p>
+                  <div class="skills">
+                    ${skills.map(skill => `<div class="skill">${skill}</div>`).join('')}
+                  </div>
+                </div>
+                <p class="personal">${personalInfo}</p>
+              </div>
+            </article>
+          </div>
+          <img src="${statusImgSrc}" alt="${statusAltText}" class="status-img" title="${statusAltText}">
+        </section>
+      `;
+
+        e.appendChild(worksheet);
+    
+
+};
+
 loadUsers(users);
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.querySelector('.search');
@@ -91,28 +134,77 @@ document.addEventListener('DOMContentLoaded', function() {
     var filter = form.querySelector('.selects');
     var filterGroup = filter.querySelector('.select-group');
     var filterStatus = filter.querySelector('.select-status');
-   
+    var usersToShow = [];
+
+    
+    // фильтр по группам
+    filterGroup.addEventListener('change', function() {
+      var groupValue = filterGroup.value;
+      usersToShow = users.filter(user => 
+        (user.group === groupValue)
+      );
+      if (usersToShow.length === 0 ) {
+        loadUsers(users); 
+      } else {
+        loadUsers(usersToShow);
+      }
+    });
   
+    // фильтр по статусу
+    filterStatus.addEventListener('change', function() {
+      var statusValue = filterStatus.value;
+      usersToShow = users.filter(user => 
+        (user.statusAltText === statusValue)
+      );
+      if (usersToShow.length === 0) {
+        loadUsers(users); 
+      } else {
+        loadUsers(usersToShow);
+      }
+    });
+  
+    // поиск по кнопке
     button.addEventListener('click', function(event) {
-      event.preventDefault(); 
-  
+      event.preventDefault();
+      
       var inp = document.querySelector('#selectedCategories input').value;
       var skillBlocks = document.querySelectorAll('.category');
       var skillsSearch = [];
+      var final = [];
+      var names = [];
 
+
+      // поиск по имени
+      names = users.filter(user =>  
+        (user.userName.trim().includes(inp))
+      );
+     
+
+      // поиск по скилам
       skillBlocks.forEach((block) => {
         skillsSearch.push(block.textContent);
       });
 
-      var usersToShow = [];
+      if (usersToShow.length == 0) {
 
-      users.forEach((user) => {
-        if (skillsSearch.some(skill => user.skills.includes(skill))) {
-          usersToShow.push(user);
-        }
-      });
+        users.forEach((user) => {
+          if (skillsSearch.some(skill => user.skills.includes(skill))) {
+            usersToShow.push(user);
+          }
+        }); 
 
-      loadUsers(usersToShow);
-      
+        loadUsers(usersToShow);
+
+      } else {
+
+        usersToShow.forEach((user) => {
+          if (skillsSearch.some(skill => user.skills.includes(skill))) {
+            final.push(user);
+          }
+        }); 
+
+        loadUsers(final);
+      }
+
     });
   });
